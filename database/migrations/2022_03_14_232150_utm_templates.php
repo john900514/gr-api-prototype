3032cea85039f63bf343b48b62b7a2fd82253918
sterlingwebb525 <sterling@capeandbay.com>
2022-03-15 12:19:22 -0400

    update down function

--- database/migrations/2022_03_14_232150_utm_templates.php
+++ database/migrations/2022_03_14_232150_utm_templates.php
@@ -35,6 +35,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('user_details');
+        Schema::dropIfExists('utm_templates');
     }
 };
