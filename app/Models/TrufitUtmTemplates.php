1869f77ed9be9eb77d105dc577855265494aace8
sterlingwebb525 <sterling@capeandbay.com>
2022-03-15 10:23:27 -0400

    Adds migrations and models for UTMS

---
+++ app/Models/TrufitUtmTemplates.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class TrufitUtmTemplates extends Model
+{
+    use SoftDeletes, Uuid;
+}
---
+++ database/migrations/2022_03_15_141018_utms_based_off_of_trufit.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trufit_utm_templates', function (Blueprint $table){
+            $table->uuid('id')->unique()->primary();
+            $table->uuid('client_id');
+            $table->uuid('utm_template_id')->nullable();
+            $table->string('entity_id');
+            $table->string('entity_name');
+            $table->string('campaign')->nullable();
+            $table->string('medium')->nullable();
+            $table->string('source')->nullable();
+            $table->string('term')->nullable();
+            $table->string('content')->nullable();
+            $table->timestamp('capture_date');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trufit_utm_templates');
+    }
+};
